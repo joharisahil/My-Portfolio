3d6920e5d6fc6583b2adcf6ea4277f0732cd4d33
JohariSahil <sahiljohari04@gmail.com>
2024-05-28 23:53:53 +0530

    added icons and bg color

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,20 @@
 import logo from "../assets/logo.png";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { MdOutlineMailOutline } from "react-icons/md";
 
 const Navbar = () => {
   return (
-    <nav className="bg-red-900 mb-20 flex items-center justify-between py-6">
+    <nav className=" mb-20 flex items-center justify-between py-6">
       <div className="flex flex-shrink-0 items-center">
         <img src={logo} alt="logo" />
+      </div>
+      <div className="m-8 flex items-center justify-center gap-8 text-4xl">
+        <FaGithub />
+        <FaLinkedin />
+        <MdOutlineMailOutline />
+        <FaInstagram />
       </div>
     </nav>
   );
